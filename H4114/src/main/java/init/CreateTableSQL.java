31c15f017679036fac882a659f82a05c89f00e9b
tianjianye <44018955+tianjianye@users.noreply.github.com>
2019-05-09 15:48:46 +0200

    sql

--- H4114/src/main/java/init/CreateTableSQL.java
+++ H4114/src/main/java/init/CreateTableSQL.java
@@ -6,6 +6,8 @@
 package init;
 
 import java.sql.Connection;
+import java.sql.DatabaseMetaData;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.logging.Level;
@@ -43,38 +45,38 @@ public class CreateTableSQL {
     }
     public static void CreateUsers(Connection conn) throws SQLException{
         Statement stmt = conn.createStatement();
-         String sql = "CREATE TABLE USERS ( ID_USER INT NOT NULL GENERATED ALWAYS AS IDENTITY (START WITH 1, INCREMENT BY 1), "
+        String sql = "CREATE TABLE USERS ( ID_USER INT NOT NULL GENERATED ALWAYS AS IDENTITY (START WITH 1, INCREMENT BY 1), "
                  + "EMAIL VARCHAR(255) NOT NULL, PSEUDO VARCHAR(255) NOT NULL, PASSWORD VARCHAR(255) NOT NULL,PRIMARY KEY (ID_USER))";
-         stmt.execute(sql);
-            /*PreparedStatement preparedStatement = conn.prepareStatement(sql);   
-            preparedStatement.executeUpdate();*/
+        if(!existe(conn,"USERS")){
+            stmt.executeUpdate(sql);
+        }
     }
     public static void CreateAssemblies(Connection conn) throws SQLException{
         Statement stmt = conn.createStatement();
-         String sql = "CREATE TABLE ASSEMBLIES( ID_ASSEMBLY INT NOT NULL GENERATED ALWAYS AS IDENTITY (START WITH 1, INCREMENT BY 1), "
+        String sql = "CREATE TABLE ASSEMBLIES( ID_ASSEMBLY INT NOT NULL GENERATED ALWAYS AS IDENTITY (START WITH 1, INCREMENT BY 1), "
                  + "TITLE VARCHAR(255) NOT NULL,DESCRIPTION VARCHAR(255) NOT NULL, DATE_TIME VARCHAR(255) NOT NULL, RADIO INT NOT NULL, "
                  + "COLOUR VARCHAR(255) NOT NULL, LATITUDE DOUBLE NOT NULL, LONGITUDE DOUBLE NOT NULL, PRIMARY KEY (ID_ASSEMBLY))";
-         stmt.execute(sql);
-          //  PreparedStatement preparedStatement = conn.prepareStatement(sql);   
-            //preparedStatement.executeUpdate();
+        if(!existe(conn,"ASSEMBLIES")){
+            stmt.executeUpdate(sql);
+        }
     }
     public static void CreateParticipants(Connection conn) throws SQLException{
         Statement stmt = conn.createStatement();
-         String sql = "CREATE TABLE PARTICIPANTS( ID_PARTICIPANT INT NOT NULL GENERATED ALWAYS AS IDENTITY (START WITH 1, INCREMENT BY 1), "
+        String sql = "CREATE TABLE PARTICIPANTS( ID_PARTICIPANT INT NOT NULL GENERATED ALWAYS AS IDENTITY (START WITH 1, INCREMENT BY 1), "
                  + "ID_USER INT NOT NULL UNIQUE,ID_ASSEMBLY INT NOT NULL, STATUS INT NOT NULL, LATITUDE DOUBLE NOT NULL, "
                  + "LONGITUDE DOUBLE NOT NULL,PRIMARY KEY (ID_PARTICIPANT),FOREIGN KEY (ID_USER) REFERENCES USERS(ID_USER),"
                  + "FOREIGN KEY (ID_ASSEMBLY) REFERENCES ASSEMBLIES(ID_ASSEMBLY))";
-        stmt.execute(sql);            
-        //PreparedStatement preparedStatement = conn.prepareStatement(sql);   
-                    //preparedStatement.executeUpdate();
+        if(!existe(conn,"PARTICIPANTS")){
+            stmt.executeUpdate(sql);
+        }            
     }
     public static void CreateSurveys(Connection conn) throws SQLException{
-         Statement stmt = conn.createStatement();
-         String sql = "CREATE TABLE SURVEYS( ID_SURVEY INT NOT NULL GENERATED ALWAYS AS IDENTITY (START WITH 1, INCREMENT BY 1), "
+        Statement stmt = conn.createStatement();
+        String sql = "CREATE TABLE SURVEYS( ID_SURVEY INT NOT NULL GENERATED ALWAYS AS IDENTITY (START WITH 1, INCREMENT BY 1), "
                  + "QUESTION VARCHAR(255) NOT NULL ,CHOICES VARCHAR(255) NOT NULL,DURATION INT NOT NULL, PRIMARY KEY(ID_SURVEY))";
-         stmt.execute(sql); 
-            //PreparedStatement preparedStatement = conn.prepareStatement(sql);   
-            //preparedStatement.executeUpdate();
+        if(!existe(conn,"SURVEYS")){
+            stmt.executeUpdate(sql);
+        }
     }
     
     public static void main (String[] args) throws SQLException{
@@ -85,5 +87,14 @@ public class CreateTableSQL {
         } catch (ClassNotFoundException | InstantiationException | IllegalAccessException ex) {
             Logger.getLogger(CreateTableSQL.class.getName()).log(Level.SEVERE, null, ex);
         }
+    }
+    
+    public static boolean existe(Connection connection, String nomTable) throws SQLException{
+	boolean existe;
+	DatabaseMetaData dmd = connection.getMetaData();
+        try (ResultSet tables = dmd.getTables(connection.getCatalog(),null,nomTable,null)) {
+            existe = tables.next();
+        }
+        return existe;	
     }
 }
